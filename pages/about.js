c34fe98e79a33d0fad87154b298c0e9a8fae9a10
Guellord26 <58660059+Guellord26@users.noreply.github.com>
2025-06-03 13:37:11 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -12,7 +12,6 @@ export default function About() {
         width={600}
         height={300}
         className="w-40 h-40 object-cover rounded-full shadow-md mb-6 md:mb-0"
-        priority
       />
         <div>
           <p className="text-gray-700 mb-4">
